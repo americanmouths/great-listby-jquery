fc09122de474686b7b6a7b493726b474bbbc2985
americanmouths <shiverr@gmail.com>
2018-04-08 19:12:12 -0700

    start show for each book

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -10,21 +10,21 @@ function Book(data){
   this.author = data.author
 }
 
-//Genre Index Prototype
+//Book Index Prototype
 Book.prototype.indexTemplate = function(){
-  let bookHTML = `<span class="glyphicon glyphicon-chevron-right"></span><a href="books/${this.id}" class="book_index">${ this.title }</a>
-  <ul><a href="#" data-id="${this.id}" class="see_book_info" id="books-${this.id}">See More Info</a></ul>`
+  let bookHTML = `<a href="#" data-id="${this.id}" class="see_book_info" id="books-${this.id}"><span class="glyphicon glyphicon-chevron-right"></span></a><a href="books/${this.id}" class="book_index">${ this.title }</a>
+  <div id="books-${this.id}"></div><br>`
   return bookHTML
 }
 
-//Get Req to Genres
+//Get Req to Books
 function bookIndex(){
   $.getJSON("/books").done(function(data){
     appendBookIndex(data)
   })
 }
 
-//Fire Ajax when Genre Link clicked in Nav Bar
+//Fire Ajax when Book Link clicked in Nav Bar
 $(document).on('turbolinks:load', function() {
   $('a.books-index-link').on('click', function(e){
     e.stopPropagation()
@@ -34,11 +34,26 @@ $(document).on('turbolinks:load', function() {
   })
 })
 
-//Append Genre Index to Page
+//Append Book Index to Page
 function appendBookIndex(data){
   data.forEach(function(bookData){
     let newBook = new Book(bookData)
     let newBookHTML = newBook.indexTemplate()
     $("#books_index").append(newBookHTML)
+  })
+}
+
+//Show Template
+Book.prototype.showTemplate = function() {
+  let bookHTML = `Written By: <u>${ this.author }</u><br>
+  Genre: <u>${ this.genre}</u>`
+  return authorHTML
+}
+
+function displayBooks(){
+  $(document).on('click', '.see_book_info', function(e){
+    e.preventDefault();
+    let id = $(this).attr('data-id')
+    $.getJSON(`/books/${id}.json`, appendBooks)
   })
 }
