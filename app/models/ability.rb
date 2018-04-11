88b87bc8c23d081535eb8944ffaf428a8ca415b9
americanmouths <shiverr@gmail.com>
2018-04-10 20:28:11 -0700

    update cancan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,6 @@ class Ability
 
   def initialize(user)
     if user.present?
-      if user.admin?
-        can :manage, :all
-      else
         can :create, BookList, :user_id => user.id
         can :read, BookList, :user_id => user.id
         can :update, BookList, :user_id => user.id
@@ -21,7 +18,6 @@ class Ability
         can :show_reviews_for, User, :id => user.id
         can :show_highest_rated_books_for, User, :id => user.id
         can :show_lowest_rated_books_for, User, :id => user.id
-      end
     else
       can :read, :reviews
       can :read, :books
